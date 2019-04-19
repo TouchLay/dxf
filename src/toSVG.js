36cee9d6216ca89f44d924d72001130843abef68
Georg Schelkshorn <georg.schelkshorn@touchlay.com>
2019-04-19 21:10:07 +0200

    Edited wrong file, add improvments

--- src/toSVG.js
+++ src/toSVG.js
@@ -27,6 +27,54 @@ const polylineToPath = (rgb, polyline) => {
   return '<path fill="none" stroke="' + hex + '" stroke-width="0.1%" d="' + d + '"/>'
 }
 
+function mtextToText(point, text, options) {
+  const { attachmentPoint, horizontalWidth, verticalHeight, nominalTextHeight } = options
+  let _point = point
+
+  /*  DXF Attachment point:
+      1 = Top left; 2 = Top center; 3 = Top right;
+      4 = Middle left; 5 = Middle center; 6 = Middle right;
+      7 = Bottom left; 8 = Bottom center; 9 = Bottom right */
+
+  // different x starting point
+  let textAnchor = 'start'
+  if ([2, 5, 8].includes(attachmentPoint))
+      textAnchor = 'middle'
+  if ([3, 6, 9].includes(attachmentPoint))
+      textAnchor = 'end'
+
+  // when we have a different y starting point we offset by the hight
+  if ([7, 8, 9].includes(attachmentPoint))
+    _point[1] = _point[1] - (nominalTextHeight || 0)
+  if ([4, 5, 6].includes(attachmentPoint)) 
+    _point[1] = _point[1] - ((nominalTextHeight || 0) / 2)
+
+  // linebreak = \\P
+  const _text = text.split('\\P')
+  const fontSize = (_text.length > 1) 
+    ? verticalHeight 
+      ? verticalHeight/_text.length 
+      : 0.5 
+    : (nominalTextHeight || 0.5)
+  
+  // todo
+  // Rotate: transform="rotate(${(options.xAxisY === 1) ? 90 : 0}deg)"
+  // Vertical Text direction: text-orientation: upright; writing-mode: vertical-rl;
+
+  return `<text 
+    x="${_point[0]}" 
+    y="${_point[1]}" 
+    width="${horizontalWidth}" 
+    height="${verticalHeight}" 
+    text-anchor="${textAnchor}"
+    style="font-size: ${fontSize}px; font-family: ${options.styleName || 'arial'};">
+      ${(_text.length > 1)
+          ? _text.map(t => `<tspan dx="0" dy="1.2em" style="font-size: ${fontSize}px;">${t}</tspan>`).join('') // FIXME needs to move dx back by -x to 0 of obove text element
+          : text
+      }
+    </text>`
+}
+
 /**
  * Convert the interpolate polylines to SVG
  */
@@ -35,6 +83,9 @@ export default (parsed) => {
   const polylines = entities.map(e => {
     return entityToPolyline(e)
   })
+
+  // TODO: better combine with polylines to avoid two loops
+  const mtexts = entities.filter(e => e.type === 'MTEXT')
 
   const bbox = new BoundingBox()
   polylines.forEach(polyline => {
@@ -65,6 +116,11 @@ export default (parsed) => {
     paths.push(polylineToPath(rgb, p2))
   })
 
+  const texts = []
+  mtexts.forEach((mtext, i) => {
+    texts.push(mtextToText([mtext.x, - (mtext.nominalTextHeight || 0) - mtext.y], mtext.string, mtext))
+  })
+
   let svgString = '<?xml version="1.0"?>'
   svgString += '<svg xmlns="http://www.w3.org/2000/svg"'
   svgString += ' xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"'
@@ -74,6 +130,6 @@ export default (parsed) => {
     (-bbox.maxY) + ' ' +
     (bbox.width) + ' ' +
     (bbox.height) + '"'
-  svgString += ' width="100%" height="100%"><g id="paths">' + paths.join('') + '</g></svg>'
+  svgString += ' width="100%" height="100%"><g id="dxf-paths">' + paths.join('') + '</g><g id="dxf-texts">' + texts.join('') + '</g></svg>'
   return pd.xml(svgString)
 }
